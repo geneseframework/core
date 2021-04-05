ac2a96da8f3ebf43c9ed7ca82bafd1ad3e533500
Genese <63037808+geneseframework@users.noreply.github.com>
2021-04-05 12:13:00 +0200

    v1.0.0-alpha.1 TAG 210405-1209 runs with @mapper v2.0.0-alpha.21 (#2)
    * Exports in index.ts
    
    * v1.0.0-alpha.1 TAG 210405-1209
    
    * v1.0.0-alpha.1 TAG 210405-1209
    
    Co-authored-by: Gilles Fabre <gillesfabre34@gmail.com>

--- src/utils/primitives/arrays.util.ts
+++ src/utils/primitives/arrays.util.ts
@@ -1,14 +1,3 @@
-export function flat(array: any[]): any[] {
-    if(!array || array.length === 0) {
-        return [];
-    }
-    else if(Array.isArray(array[0])) {
-        return flat(array[0]).concat(flat(array.slice(1)));
-    }
-    else {
-        return [array[0]].concat(flat(array.slice(1)));
-    }
-}
 
 /**
  * Checks if all the elements of the small array are in the big array
@@ -22,22 +11,52 @@ export function includes(bigArray: any[], smallArray: any[]): boolean {
     return !smallArray.some(element => !bigArray.includes(element));
 }
 
-
-export function isArray(data: any): data is Array<any> {
-    return Array.isArray(data);
+/**
+ * Checks if a value is a boolean
+ * @param value     // The value to check
+ */
+export function isArray(value: any): value is Array<any> {
+    return Array.isArray(value);
 }
 
-
+/**
+ * Checks if all the elements of a given array are arrays
+ * @param args
+ */
 export function areArrays(...args: any[][]): boolean {
     return args.every(a => isArray(a));
 }
 
-
+/**
+ * Checks if two values are both arrays or both not arrays
+ * @param first     // The first value
+ * @param last      // The last value
+ */
 export function areNotArrays(first: any, last: any): boolean {
     return !isArray(first) || !isArray(last);
 }
 
-
+/**
+ * Checks if two arrays have same length
+ * @param array1
+ * @param array2
+ */
 export function haveSameLength(array1: any[], array2: any[]): boolean {
     return areArrays(array1, array2) && array1.length === array2.length;
+}
+
+/**
+ * Returns a flatten array from a given array
+ * @param array     // The array to flat
+ */
+export function flat(array: any[]): any[] {
+    if(!array || array.length === 0) {
+        return [];
+    }
+    else if(Array.isArray(array[0])) {
+        return flat(array[0]).concat(flat(array.slice(1)));
+    }
+    else {
+        return [array[0]].concat(flat(array.slice(1)));
+    }
 }
